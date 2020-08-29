a41250753431de6440115ef0378646c9218006b9
Rosi <rosaorellana2306@gmail.com>
2020-08-29 12:31:28 -0600

    Primer avance conversor

---
+++ app/src/main/java/com/example/conversorees/Masa.java
@@ -0,0 +1,21 @@
+package com.example.conversorees;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class Masa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_masa);
+
+
+
+    }
+}
