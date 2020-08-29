577fcf3b7673a75eb56c2be1da52c591a9ecb353
YasserHimede <60446463+YasserHimede@users.noreply.github.com>
2020-08-29 15:34:52 -0600

    Segundo Avance Conversor

--- app/src/main/java/com/example/conversorees/Transdatos.java
+++ app/src/main/java/com/example/conversorees/Transdatos.java
@@ -15,6 +15,30 @@ public class Transdatos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transdatos);
 
+        Button button = (Button)findViewById(R.id.idconvertir);
+    }
+
+    public void transferencia(View v){
+        try{
+            TextView tempVal=(TextView)findViewById(R.id.edtCantidad);
+            Double cantidad3=Double.parseDouble(tempVal.getText().toString());
+
+            Spinner spn=(Spinner)findViewById(R.id.cboDato);
+            Integer de =spn.getSelectedItemPosition();
+
+            Spinner spna=(Spinner)findViewById(R.id.cboDatos);
+            Integer a =spna.getSelectedItemPosition();
+
+            Double[]unidades={1e-9,1e-6,0.000125,1e-12};
+
+            Double valor4=unidades[a]/unidades[de]*cantidad3;
+            tempVal=(TextView)findViewById(R.id.txtRespuesta);
+            tempVal.setText("Respuesta:"+valor4);
+        }catch(Exception error){
+            TextView tempVal = (TextView) findViewById(R.id.txtRespuesta);
+            tempVal.setText("Ingrese la Cantidad a Convertir ");
+        }
+
 
 
     }
